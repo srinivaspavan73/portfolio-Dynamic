daa90c32a11692086881a495223cf3a8156e4542
Bitra Pavan Kumar <100582064+srinivaspavan73@users.noreply.github.com>
2024-08-20 19:23:10 +0530

    initial Commit

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -3,9 +3,9 @@ export const contactsData = {
     phone: '+918555866173',
     address: 'Chirala, Andhra Pradesh, India',
     github: 'https://github.com/srinivaspavan73',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
+    facebook: 'https://www.facebook.com/bitra.pavan.351',
+    linkedIn: 'https://www.linkedin.com/in/bitra-pavan-kumar-8ba067226/',
+    twitter: 'https://twitter.com/srinivaspavan73',
     medium: 'https://medium.com/@abusaid-bd',
     stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
     devUsername: "said7388"
