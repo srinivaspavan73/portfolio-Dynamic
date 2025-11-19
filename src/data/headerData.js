c9ec0fec22a925b9a4309eb8f0e3de4d175bd66a
srinivaspavan73 <bitrasrinivaspavan@gmail.com>
2025-11-19 12:47:27 +0530

    surname changed

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import dp from '../assets/png/dp.jpg'
 export const headerData = {
     name: 'Pavan Kumar Bitra',
     title: "Hello! I am",
-    desciption: "Hello! I’m Bitra Pavan Kumar, a passionate full-stack developer with a strong focus on continuous learning and problem-solving. I thrive in dynamic environments where I can face new challenges and apply innovative solutions.Driven by curiosity, I’m always eager to learn new technologies and expand my skill set. I believe in creating meaningful, high-impact projects, and I am dedicated to delivering results through efficient, scalable, and well-structured solutions.",
+    desciption: "Hello! I’m Pavan Kumar Bitra, a passionate full-stack developer with a strong focus on continuous learning and problem-solving. I thrive in dynamic environments where I can face new challenges and apply innovative solutions.Driven by curiosity, I’m always eager to learn new technologies and expand my skill set. I believe in creating meaningful, high-impact projects, and I am dedicated to delivering results through efficient, scalable, and well-structured solutions.",
     image: dp,
     // imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
     // resumePdf: 'https://drive.google.com/file/d/1MK9UbJqSfFFtXHQf1OjTltbTCmNrEFPR/view?usp=sharing'
