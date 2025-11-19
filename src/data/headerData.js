20291661553b69634df8fb2936f1b8d8dfb41488
srinivaspavan73 <bitrasrinivaspavan@gmail.com>
2025-11-19 12:46:14 +0530

    resume updated

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import dp from '../assets/png/dp.jpg'
 export const headerData = {
-    name: 'Bitra Pavan Kumar',
+    name: 'Pavan Kumar Bitra',
     title: "Hello! I am",
     desciption: "Hello! I’m Bitra Pavan Kumar, a passionate full-stack developer with a strong focus on continuous learning and problem-solving. I thrive in dynamic environments where I can face new challenges and apply innovative solutions.Driven by curiosity, I’m always eager to learn new technologies and expand my skill set. I believe in creating meaningful, high-impact projects, and I am dedicated to delivering results through efficient, scalable, and well-structured solutions.",
     image: dp,
     // imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
-    resumePdf: 'https://drive.google.com/file/d/1MK9UbJqSfFFtXHQf1OjTltbTCmNrEFPR/view?usp=sharing'
+    // resumePdf: 'https://drive.google.com/file/d/1MK9UbJqSfFFtXHQf1OjTltbTCmNrEFPR/view?usp=sharing'
+    resumePdf: 'https://drive.google.com/file/d/1I9YrkeTuUo1XDtCmYx3Z0ksRwbVaWiWf/view?usp=sharing'
 }
